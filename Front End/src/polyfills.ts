0627c1cd9ceb8cd1400437aabf69739e56ac14b1
Harshal Mulay <harshalmulay1039@gmail.com>
2025-09-16 18:10:48 +0530

    deploing changes

---
+++ Front End/src/polyfills.ts
@@ -0,0 +1,12 @@
+/***************************************************************************************************
+ * BROWSER POLYFILLS
+ */
+
+/** IE11 requires all of the following polyfills. **/
+/** import 'core-js/es/array'; **/
+/** import 'core-js/es/object'; **/
+
+/***************************************************************************************************
+ * Zone JS is required by Angular itself.
+ */
+import 'zone.js';  // Included with Angular CLI.
